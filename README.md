f6d3ad1a6c26053e6cf0c0782d39799a68cb7d2e
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2024-05-09 11:25:47 +0300

    Added a new operation, fixed some bugs and updated readme file.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # E-Diary
-This is a simple console app that can add grades to a specific subject, given by the user. Still in progress.
+This is a simple console app that can add grades to a specific subject, given by the user.
 
 ## Grading scale:
 The grading scale is based on the Bulgarian Grading System.
@@ -14,4 +14,15 @@ After validating the email, you can do one of the following operations:
 - Show subjects;
 - Add subjects;
 - Add grade;
+- Sort subject from highest to lowest average grade
 - Quit.
+
+# Versions:
+## 1.0:
+A simple console application which can perform the operations showed above.
+## 2.0:
+In progress
+
+# Purpose:
+The purpose of this project is to practice Git and GitHub skills, to practice dictionaries, tuples, and OOP. I used the
+"re" library for the email validation.
--- e_diary.py
+++ e_diary.py
@@ -31,37 +31,11 @@ class Electronic_Diary():
 
         return average_grade
 
-    def show_subjects_and_grades(self):
-        if self.subjects:
-            print("Subjects:")
+    def sort_subjects(self, subject_and_average_grade):
+        subject_and_average_grade = dict(sorted(subject_and_average_grade.items(), key=lambda item: item[1]))
+        subject_and_average_grade = dict(reversed(list(subject_and_average_grade.items())))
 
-            all_average_grades = ()
-
-            for subject, grades in self.subjects.items():
-                total_grades = list(map(float, grades))  # This is the same as the grades variable, but it is a float.
-
-                if grades:
-                    average_grade = self.calculate_average_grade(total_grades)
-                    print(f"{subject}: {", ".join(grades)} | Average grade: {average_grade:.2f}")
-
-                    all_average_grades += (average_grade,)
-                else:
-                    print(f"{subject}: no grades")
-
-            total_average_grade = self.calculate_average_grade(all_average_grades)
-
-            print(f"----------\nTotal: {total_average_grade}")
-        else:
-            print("No subjects entered.")
-
-    def show_subjects(self):
-        number_of_subject = 1
-
-        for subject in self.subjects:
-            print(f"{number_of_subject} - {subject}")
-            number_of_subject += 1
-
-        print()  # To look prettier on the console
+        return subject_and_average_grade
 
     def add_subject(self, subject):
         self.subjects[subject] = ()
@@ -77,7 +51,59 @@ class Electronic_Diary():
         else:
             print("Invalid grade and subject not found!")
 
-        print(f"\nGrade added successfully!")
+    def show_subjects_and_grades(self):
+
+        if self.subjects:
+            show_average_grade = False
+
+            print("Subjects:")
+
+            all_average_grades = ()
+
+            for subject, grades in self.subjects.items():
+                total_grades = list(map(float, grades))  # This is the same as the grades variable, but it is a float.
+
+                if grades:
+                    average_grade = self.calculate_average_grade(total_grades)
+                    print(f"{subject}: {", ".join(grades)} | Average grade: {average_grade:.2f}")
+
+                    all_average_grades += (average_grade,)
+                    show_average_grade = True  # If not it may raise an error when printed the total
+                else:
+                    print(f"{subject}: no grades")
+
+            if show_average_grade:
+                total_average_grade = self.calculate_average_grade(all_average_grades)
+
+                print(f"----------\nTotal: {total_average_grade}")
+        else:
+            print("No subjects entered.")
+
+    def show_sorted_subjects(self):
+        subject_and_average_grade = {}
+
+        for subject, grades in self.subjects.items():
+            total_grades = list(map(float, grades))
+
+            subject_and_average_grade[subject] = self.calculate_average_grade(total_grades)
+
+        subject_and_average_grade = self.sort_subjects(subject_and_average_grade)
+
+        place = 1
+
+        for subject, average_grade in subject_and_average_grade.items():
+            print(f"{place}. {subject} - {average_grade}")
+
+            place += 1
+
+    def show_subjects(self):
+        number_of_subject = 1
+
+        for subject in self.subjects:
+            print(f"{number_of_subject} - {subject}")
+            number_of_subject += 1
+
+        print()  # To look prettier on the console
 
 
 def main():
@@ -102,7 +128,8 @@ def main():
     # Operations
     while True:
         print(f"\nChose one of the following operations:")
-        print("1) Show subject \n2) Add subjects \n3) Add grade \n4) Quit\n")
+        print("1) Show subject \n2) Add subjects \n3) Add grade \n"
+              "4) Sort subject from highest to lowest average grade \n5) Quit\n")
 
         choice = int(input("Enter the number of the operation: "))
 
@@ -112,7 +139,10 @@ def main():
         elif choice == 2:  # Add subject
             subject = input("\nEnter the subject that you would like to add: \n")
 
-            user_diary.add_subject(subject)
+            if user_diary.subject_validation(subject):
+                print("\nSubject already added!")
+            else:
+                user_diary.add_subject(subject)
         elif choice == 3:  # Add grade
             print()  # To look prettier on the console
 
@@ -132,10 +162,17 @@ def main():
                         print("Subject not found!")
             else:
                 print("No subject in the diary.")
-        elif choice == 4:  # Quit
-            print(f"------------------------------------"
+        elif choice == 4:  # Sort subject from highest to lowest average grade
+            if user_diary.subject_presence():
+                user_diary.show_sorted_subjects()
+            else:
+                print("No subject in the diary.")
+        elif choice == 5:  # Quit
+            print(f"\n------------------------------------"
                   f"\nGoodbye, {username}! Have a great day!")
             break
+        else:
+            print("Invalid operation!")
 
 
 if __name__ == "__main__":
